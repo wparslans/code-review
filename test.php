0df1aecc4c882b4658d0a6043e332635d1df5c1e
wparslans <muhammad.arslan@ocuco.com>
2025-10-02 11:28:17 +0500

    testing

--- test.php
+++ test.php
@@ -9,13 +9,12 @@ Author: Someone
 global $wpdb;
 $results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}users WHERE user_login = '" . $_GET['user'] . "'");
 
+function BadFunctionNAME($Param) {
+    echo "Bad style! " . $Param;
+}
+add_action('init', 'BadFunctionNAME');
 if ( isset($_POST['my_form_submit']) ) {
     update_option('my_plugin_option', $_POST['my_input']);
 }
 
 echo '<h1>' . $_GET['title'] . '</h1>';
-
-function BadFunctionNAME($Param) {
-    echo "Bad style! " . $Param;
-}
-add_action('init', 'BadFunctionNAME');
